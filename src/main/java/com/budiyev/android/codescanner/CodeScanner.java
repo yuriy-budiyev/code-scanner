fced5cb56731aedd52213e3ddea58f4ff64256ef
Yuriy Budiyev <y.budiyev@corp.mail.ru>
2017-08-15 14:02:47 +0300

    Dev

--- src/main/java/com/budiyev/android/codescanner/CodeScanner.java
+++ src/main/java/com/budiyev/android/codescanner/CodeScanner.java
@@ -30,6 +30,8 @@ import android.os.Handler;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
+import android.support.v4.view.ViewCompat;
 import android.view.SurfaceHolder;
 
 import com.google.zxing.BarcodeFormat;
@@ -50,7 +52,8 @@ public final class CodeScanner {
                     BarcodeFormat.MAXICODE, BarcodeFormat.PDF_417, BarcodeFormat.QR_CODE,
                     BarcodeFormat.RSS_14, BarcodeFormat.RSS_EXPANDED, BarcodeFormat.UPC_A,
                     BarcodeFormat.UPC_E, BarcodeFormat.UPC_EAN_EXTENSION);
-    private final Lock mLock = new ReentrantLock();//TODO
+    private static final int UNSPECIFIED = -1;
+    private final Lock mInitializeLock = new ReentrantLock();
     private final Context mContext;
     private final Handler mMainThreadHandler;
     private final CodeScannerView mScannerView;
@@ -58,20 +61,19 @@ public final class CodeScanner {
     private final Camera.PreviewCallback mPreviewCallback;
     private final SurfaceHolder mSurfaceHolder;
     private final int mCameraId;
+    private volatile DecodeCallback mDecodeCallback;
     private volatile Camera mCamera;
     private volatile Decoder mDecoder;
-    private volatile DecodeCallback mDecodeCallback;
     private volatile Point mPreviewSize;
+    private volatile Point mFrameSize;
     private volatile int mDisplayOrientation;
-    private volatile int mFrameWidth;
-    private volatile int mFrameHeight;
-    private volatile boolean mPreviewRequested;
+    private volatile boolean mInitialization;
     private volatile boolean mInitialized;
     private volatile boolean mPreviewActive;
 
     @MainThread
     public CodeScanner(@NonNull Context context, @NonNull CodeScannerView view) {
-        this(context, view, -1);
+        this(context, view, UNSPECIFIED);
     }
 
     @MainThread
@@ -84,13 +86,6 @@ public final class CodeScanner {
         mSurfaceHolder = view.getPreviewView().getHolder();
         mCameraId = cameraId;
         setFormats(ALL_FORMATS);
-        int width = view.getWidth();
-        int height = view.getHeight();
-        if (width == 0 && height == 0) {
-            view.setLayoutListener(new ScannerLayoutListener());
-        } else {
-            initialize(width, height);
-        }
     }
 
     public void setFormats(@NonNull List<BarcodeFormat> formats) {
@@ -105,14 +100,49 @@ public final class CodeScanner {
         mDecodeCallback = decodeCallback;
     }
 
+    private void initialize() {
+        if (ViewCompat.isLaidOut(mScannerView)) {
+            initialize(mScannerView.getWidth(), mScannerView.getHeight());
+        } else {
+            mScannerView.setLayoutListener(new ScannerLayoutListener());
+        }
+    }
+
     private void initialize(int width, int height) {
         new InitializeThread(width, height).start();
     }
 
+    @WorkerThread
+    private void finishInitialization(@NonNull Camera camera, @NonNull Point previewSize,
+            @NonNull Point frameSize, int displayOrientation) {
+        mInitializeLock.lock();
+        try {
+            mCamera = camera;
+            mDecoder = new Decoder(new DecoderStateListener());
+            mPreviewSize = previewSize;
+            mFrameSize = frameSize;
+            mDisplayOrientation = displayOrientation;
+            mInitialization = false;
+            mInitialized = true;
+        } finally {
+            mInitializeLock.unlock();
+        }
+        mMainThreadHandler.post(() -> {
+            mScannerView.setFrameSize(frameSize);
+            startPreview();
+        });
+    }
+
     public void startPreview() {
-        if (!isInitialized()) {
-            mPreviewRequested = true;
-            return;
+        mInitializeLock.lock();
+        try {
+            if (!mInitialized && !mInitialization) {
+                mInitialization = true;
+                initialize();
+                return;
+            }
+        } finally {
+            mInitializeLock.unlock();
         }
         SurfaceHolder surfaceHolder = mSurfaceHolder;
         if (!mPreviewActive && surfaceHolder != null) {
@@ -123,10 +153,8 @@ public final class CodeScanner {
     }
 
     public void stopPreview() {
-        mPreviewRequested = false;
-        SurfaceHolder surfaceHolder = mSurfaceHolder;
-        if (isInitialized() && mPreviewActive && surfaceHolder != null) {
-            surfaceHolder.removeCallback(mSurfaceCallback);
+        if (mInitialized && mPreviewActive) {
+            mSurfaceHolder.removeCallback(mSurfaceCallback);
             mCamera.setPreviewCallback(null);
             mCamera.stopPreview();
             mPreviewActive = false;
@@ -134,30 +162,10 @@ public final class CodeScanner {
     }
 
     public void releaseResources() {
-        mPreviewRequested = false;
         mPreviewActive = false;
-        if (!isInitialized()) {
-            return;
-        }
-        mCamera.release();
-        mDecoder.shutdown();
-    }
-
-    private boolean isInitialized() {
-        mLock.lock();
-        try {
-            return mInitialized;
-        } finally {
-            mLock.unlock();
-        }
-    }
-
-    private void setInitialized(boolean initialized) {
-        mLock.lock();
-        try {
-            mInitialized = initialized;
-        } finally {
-            mLock.unlock();
+        if (mInitialized) {
+            mCamera.release();
+            mDecoder.shutdown();
         }
     }
 
@@ -172,16 +180,12 @@ public final class CodeScanner {
     private class PreviewCallback implements Camera.PreviewCallback {
         @Override
         public void onPreviewFrame(byte[] data, Camera camera) {
+            if (!mInitialized || mDecoder.isDecoding()) {
+                return;
+            }
             Point previewSize = mPreviewSize;
-            if (previewSize == null || !isInitialized() || mDecoder.isDecoding()) {
-                return;
-            }
-            int frameWidth = mFrameWidth;
-            int frameHeight = mFrameHeight;
-            if (frameWidth == 0 || frameHeight == 0) {
-                return;
-            }
-            mDecoder.decode(data, previewSize.x, previewSize.y, frameWidth, frameHeight,
+            Point frameSize = mFrameSize;
+            mDecoder.decode(data, previewSize.x, previewSize.y, frameSize.x, frameSize.y,
                     mDisplayOrientation, mScannerView.isSquareFrame(), mDecodeCallback);
         }
     }
@@ -250,47 +254,38 @@ public final class CodeScanner {
         @Override
         @SuppressWarnings("SuspiciousNameCombination")
         public void run() {
-            Camera camera;
-            if (mCameraId == -1) {
-                camera = Camera.open();
+            Camera camera = null;
+            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+            if (mCameraId == UNSPECIFIED) {
+                int numberOfCameras = Camera.getNumberOfCameras();
+                for (int i = 0; i < numberOfCameras; i++) {
+                    Camera.getCameraInfo(i, cameraInfo);
+                    if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                        camera = Camera.open(i);
+                        break;
+                    }
+                }
             } else {
                 camera = Camera.open(mCameraId);
+                Camera.getCameraInfo(mCameraId, cameraInfo);
             }
             if (camera == null) {
                 throw new RuntimeException("Unable to access camera");
             }
-            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-            Camera.getCameraInfo(mCameraId, cameraInfo);
             Camera.Parameters parameters = camera.getParameters();
-            Point frameSize;
-            if (parameters != null) {
-                boolean portrait = ScannerHelper.isPortrait(mDisplayOrientation);
-                Point previewSize = CameraConfigurationUtils.findBestPreviewSizeValue(parameters,
-                        portrait ? new Point(mHeight, mWidth) : new Point(mWidth, mHeight));
-                mPreviewSize = previewSize;
-                parameters.setPreviewSize(previewSize.x, previewSize.y);
-                frameSize = ScannerHelper.getFrameSize(portrait ? previewSize.y : previewSize.x,
-                        portrait ? previewSize.x : previewSize.y, mWidth, mHeight);
-                camera.setParameters(ScannerHelper.optimizeParameters(parameters));
-            } else {
-                frameSize = null;
+            if (parameters == null) {
+                throw new RuntimeException("Unable to configure camera");
             }
+            boolean portrait = ScannerHelper.isPortrait(mDisplayOrientation);
+            Point previewSize = CameraConfigurationUtils.findBestPreviewSizeValue(parameters,
+                    portrait ? new Point(mHeight, mWidth) : new Point(mWidth, mHeight));
+            parameters.setPreviewSize(previewSize.x, previewSize.y);
+            Point frameSize = ScannerHelper.getFrameSize(portrait ? previewSize.y : previewSize.x,
+                    portrait ? previewSize.x : previewSize.y, mWidth, mHeight);
+            camera.setParameters(ScannerHelper.optimizeParameters(parameters));
             int orientation = ScannerHelper.getDisplayOrientation(mContext, cameraInfo.orientation);
-            mDisplayOrientation = orientation;
             camera.setDisplayOrientation(orientation);
-            mFrameWidth = mWidth;
-            mFrameHeight = mHeight;
-            mCamera = camera;
-            mDecoder = new Decoder(new DecoderStateListener());
-            mMainThreadHandler.post(() -> {
-                if (frameSize != null) {
-                    mScannerView.setFrameSize(frameSize);
-                }
-                setInitialized(true);
-                if (mPreviewRequested) {
-                    startPreview();
-                }
-            });
+            finishInitialization(camera, previewSize, frameSize, orientation);
         }
     }
 }
