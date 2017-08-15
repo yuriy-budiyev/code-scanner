2d1eea35716959d7d19c0c2e9552ec6e0c0d89f9
Yuriy Budiyev <y.budiyev@corp.mail.ru>
2017-08-15 21:11:14 +0300

    Dev

--- src/main/java/com/budiyev/android/codescanner/CodeScannerView.java
+++ src/main/java/com/budiyev/android/codescanner/CodeScannerView.java
@@ -40,7 +40,7 @@ public class CodeScannerView extends ViewGroup {
     private static final boolean DEFAULT_SQUARE_FRAME = false;
     private static final int DEFAULT_MASK_COLOR = 0x77000000;
     private static final int DEFAULT_FRAME_COLOR = Color.WHITE;
-    private static final float DEFAULT_FRAME_WIDTH_DP = 3f;
+    private static final float DEFAULT_FRAME_WIDTH_DP = 2f;
     private static final float DEFAULT_FRAME_CORNER_SIZE_DP = 50f;
     private SurfaceView mPreviewView;
     private ViewFinderView mViewFinderView;
