d677883fcd63c12466e1bceef83164b94c6e8959
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2017-08-16 23:15:09 +0300

    Doc

--- src/main/java/com/budiyev/android/codescanner/DecodeCallback.java
+++ src/main/java/com/budiyev/android/codescanner/DecodeCallback.java
@@ -23,12 +23,28 @@
  */
 package com.budiyev.android.codescanner;
 
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
 import com.google.zxing.Result;
 
+/**
+ * Callback of the decoding process
+ */
 public interface DecodeCallback {
+    /**
+     * Called when decoder has successfully decoded the code
+     * <br>
+     * Note that this method is called on a worker thread
+     *
+     * @param result Encapsulates the result of decoding a barcode within an image
+     * @see Handler
+     * @see Looper#getMainLooper()
+     * @see Activity#runOnUiThread(Runnable)
+     */
     @WorkerThread
     void onDecoded(@NonNull Result result);
 }
