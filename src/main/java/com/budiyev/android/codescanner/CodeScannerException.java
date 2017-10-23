3d2af9a646f4bb6a4908b59c8972de6c7da60da9
Yuriy Budiyev <y.budiyev@corp.mail.ru>
2017-10-23 13:10:55 +0300

    Dev

--- src/main/java/com/budiyev/android/codescanner/CodeScannerException.java
+++ src/main/java/com/budiyev/android/codescanner/CodeScannerException.java
@@ -31,71 +31,22 @@ import android.support.annotation.RequiresApi;
  * Common class for code scanner runtime exceptions
  */
 public class CodeScannerException extends RuntimeException {
-    /**
-     * Constructs a new exception with {@code null} as its detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause(Throwable)}.
-     */
     public CodeScannerException() {
         super();
     }
 
-    /**
-     * Constructs a new exception with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause(Throwable)}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
     public CodeScannerException(@Nullable String message) {
         super(message);
     }
 
-    /**
-     * Constructs a new exception with the specified detail message and
-     * cause.
-     * Note that the detail message associated with {@code cause} is not
-     * automatically incorporated in this exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A {@code null} value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
-     */
     public CodeScannerException(@Nullable String message, @Nullable Throwable cause) {
         super(message, cause);
     }
 
-    /**
-     * Constructs a new exception with the specified cause and a detail
-     * message of {@code (cause==null ? null : cause.toString())} (which
-     * typically contains the class and detail message of {@code cause}).
-     *
-     * @param cause the cause (which is saved for later retrieval by the
-     *              {@link #getCause()} method).  (A {@code null} value is
-     *              permitted, and indicates that the cause is nonexistent or
-     *              unknown.)
-     */
     public CodeScannerException(@Nullable Throwable cause) {
         super(cause);
     }
 
-    /**
-     * Constructs a new exception with the specified detail message,
-     * cause, suppression enabled or disabled, and writable stack
-     * trace enabled or disabled.
-     *
-     * @param message            the detail message.
-     * @param cause              the cause.  (A {@code null} value is permitted,
-     *                           and indicates that the cause is nonexistent or unknown.)
-     * @param enableSuppression  whether or not suppression is enabled
-     *                           or disabled
-     * @param writableStackTrace whether or not the stack trace should
-     *                           be writable
-     */
     @RequiresApi(Build.VERSION_CODES.N)
     protected CodeScannerException(@Nullable String message, @Nullable Throwable cause,
             boolean enableSuppression, boolean writableStackTrace) {
