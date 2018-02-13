aa03daaf27dca56d0acf7bd305ab808341450ed6
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2018-02-13 11:59:31 +0300

    Dev

--- src/main/java/com/budiyev/android/codescanner/CodeScannerActivity.java
+++ src/main/java/com/budiyev/android/codescanner/CodeScannerActivity.java
@@ -1,18 +1,12 @@
 package com.budiyev.android.codescanner;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.ViewGroup;
@@ -149,149 +143,12 @@ public class CodeScannerActivity extends Activity {
     }
 
     /**
-     * Intent builder, to configure scanner activity
-     */
-    @NonNull
-    public static IntentBuilder intent() {
-        return new IntentBuilder();
-    }
-
-    /**
-     * Default intent
+     * Intent to start scanner activity
      *
      * @param context Package context
      */
     @NonNull
     public static Intent intent(@NonNull Context context) {
         return new Intent(context, CodeScannerActivity.class);
-    }
-
-    public static final class IntentBuilder {
-        private int mCameraId = CodeScanner.DEFAULT_CAMERA;
-        private List<BarcodeFormat> mFormats = CodeScanner.DEFAULT_FORMATS;
-        private boolean mAutoFocusEnabled = CodeScanner.DEFAULT_AUTO_FOCUS_ENABLED;
-        private ScanMode mScanMode = CodeScanner.DEFAULT_SCAN_MODE;
-        private AutoFocusMode mAutoFocusMode = CodeScanner.DEFAULT_AUTO_FOCUS_MODE;
-        private long mAutoFocusInterval = CodeScanner.DEFAULT_SAFE_AUTO_FOCUS_INTERVAL;
-        private boolean mFlashEnabled = CodeScanner.DEFAULT_FLASH_ENABLED;
-
-        private IntentBuilder() {
-        }
-
-        /**
-         * Camera that will be used by scanner.
-         * First back-facing camera on the device by default.
-         *
-         * @param cameraId Camera id (between {@code 0} and
-         *                 {@link Camera#getNumberOfCameras()} - {@code 1})
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder camera(int cameraId) {
-            mCameraId = cameraId;
-            return this;
-        }
-
-        /**
-         * Formats, decoder to react to ({@link CodeScanner#ALL_FORMATS} by default)
-         *
-         * @param formats Formats
-         * @see BarcodeFormat
-         * @see CodeScanner#ALL_FORMATS
-         * @see CodeScanner#ONE_DIMENSIONAL_FORMATS
-         * @see CodeScanner#TWO_DIMENSIONAL_FORMATS
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder formats(@NonNull List<BarcodeFormat> formats) {
-            mFormats = formats;
-            return this;
-        }
-
-        /**
-         * Formats, decoder to react to ({@link CodeScanner#ALL_FORMATS} by default)
-         *
-         * @param formats Formats
-         * @see BarcodeFormat
-         * @see CodeScanner#ALL_FORMATS
-         * @see CodeScanner#ONE_DIMENSIONAL_FORMATS
-         * @see CodeScanner#TWO_DIMENSIONAL_FORMATS
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder formats(@NonNull BarcodeFormat... formats) {
-            mFormats = Arrays.asList(formats);
-            return this;
-        }
-
-        /**
-         * Format, decoder to react to
-         *
-         * @param format Format
-         * @see BarcodeFormat
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder format(@NonNull BarcodeFormat format) {
-            mFormats = Collections.singletonList(format);
-            return this;
-        }
-
-        /**
-         * Whether to enable or disable auto focus if it's supported, {@code true} by default
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder autoFocus(boolean enabled) {
-            mAutoFocusEnabled = enabled;
-            return this;
-        }
-
-        /**
-         * Set auto focus mode, {@link AutoFocusMode#SAFE} by default
-         *
-         * @see AutoFocusMode
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder autoFocusMode(@NonNull AutoFocusMode mode) {
-            mAutoFocusMode = mode;
-            return this;
-        }
-
-        /**
-         * Set auto focus interval in milliseconds for {@link AutoFocusMode#SAFE} mode,
-         * 2000 by default
-         *
-         * @see #autoFocusMode(AutoFocusMode)
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder autoFocusInterval(long interval) {
-            mAutoFocusInterval = interval;
-            return this;
-        }
-
-        /**
-         * Whether to enable or disable flash light if it's supported, {@code false} by default
-         */
-        @NonNull
-        @MainThread
-        public IntentBuilder flash(boolean enabled) {
-            mFlashEnabled = enabled;
-            return this;
-        }
-
-        /**
-         * Create new {@link CodeScanner} instance with specified parameters
-         *
-         * @param context Context
-         * @see CodeScannerView
-         */
-        @NonNull
-        @MainThread
-        public Intent build(@NonNull Context context) {
-            return new Intent(context, CodeScannerActivity.class);
-        }
     }
 }
